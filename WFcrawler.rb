9f41ddaf81d3a3adc0b31b1c6b2ff975801a5871
Alvadine <johnbs1234@gmail.com>
2019-02-10 11:09:23 -0500

    fix(functional): website had different architecture for 2012 pages, updated code to read the url and select the appropriate method.

--- WFcrawler.rb
+++ WFcrawler.rb
@@ -5,11 +5,16 @@ site = ARGV
 driver.get(site[0])
 sleep(2)
 dir = driver.title()
-FileUtils.mkdir_p dir 
-driver.find_element(:id, "btnToBegining").click
-containerFrame = driver.find_element(:class => "ui-slider-track")
+if !site[0].include? "2012" 
+	driver.find_element(:id, "btnToBegining").click
+	containerFrame = driver.find_element(:class => "ui-slider-track")
+	slider = driver.find_element(:class => "ui-slider-handle")
+else
+	driver.find_element(:class, "ui-icon-seek-start").click
+	containerFrame = driver.find_element(:class => "ui-slider-horizontal")
+	slider = driver.find_element(:class => "ui-slider-handle")
+end
 width = containerFrame.size.width
-slider = driver.find_element(:class => "ui-slider-handle")
 i=0
 while i < 100
 	driver.action.drag_and_drop_by(slider,width/100,0).perform
@@ -17,6 +22,7 @@ while i < 100
 	sleep(0.5)
 	i += 1
 end
+FileUtils.mkdir_p dir 
 dir="mv *.png " + dir.gsub(" ","\\ ")
 system(dir)
 exit
