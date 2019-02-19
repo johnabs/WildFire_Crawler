6bd9c1641b54e613b27b2275f5a0fc472bc5f599
Alvadine <johnbs1234@gmail.com>
2019-02-19 18:27:11 -0500

    fix(comments):added comments for easier to read code.

--- WFcrawler.rb
+++ WFcrawler.rb
@@ -1,19 +1,25 @@
 require 'selenium-webdriver'
 require 'fileutils'
+#Open new browser
 driver = Selenium::WebDriver.for :chrome 
+#Pass website as command line argument
 site = ARGV
+#Open Website and wait for it to load
 driver.get(site[0])
 sleep(2)
 dir = driver.title()
+#Check to see if link contains the year 2012, as the web formatting is drastically different
 if !site[0].include? "2012" 
 	driver.find_element(:id, "btnToBegining").click
 	containerFrame = driver.find_element(:class => "ui-slider-track")
 	slider = driver.find_element(:class => "ui-slider-handle")
+#Otherwise, use the normal script to find the relevant UI parts 
 else
 	driver.find_element(:class, "ui-icon-seek-start").click
 	containerFrame = driver.find_element(:class => "ui-slider-horizontal")
 	slider = driver.find_element(:class => "ui-slider-handle")
 end
+#Get width of the frame, divide by 100 later so we move the slider by 1% each time
 width = containerFrame.size.width
 i=0
 while i < 100
@@ -22,6 +28,7 @@ while i < 100
 	sleep(0.5)
 	i += 1
 end
+# Makes new folder named after the web directory, and moves all the screenshotted images to that directory (defaults to ALL png files)
 FileUtils.mkdir_p dir 
 dir="mv *.png " + dir.gsub(" ","\\ ")
 system(dir)
